916d0375053590d8f37270dd28cfb75a8d94b1e8
H. Turgut Uyar <uyar@tekir.org>
2022-10-03 11:37:34 +0300

    use html normalizer for xml conversion

--- kirlent/tasks.py
+++ kirlent/tasks.py
@@ -1,3 +1,7 @@
+from contextlib import redirect_stdout
+from html import escape as html_escape
+from html.parser import HTMLParser
+from io import StringIO
 from itertools import dropwhile, zip_longest
 from pathlib import Path
 from urllib.parse import urlparse
@@ -43,9 +47,49 @@ def relative_path(path, wrt=None):
     return Path(up_path, down_path)
 
 
+class HTMLNormalizer(HTMLParser):
+    VOID_ELEMENTS = frozenset({"br", "col", "hr", "img", "link", "meta"})
+
+    def handle_starttag(self, tag, attrs):
+        attribs = []
+        for attr_name, attr_value in attrs:
+            if attr_value is None:
+                attr_value = ""
+            markup = '%(name)s="%(value)s"' % {
+                "name": attr_name,
+                "value": html_escape(attr_value, quote=True),
+            }
+            attribs.append(markup)
+        line = "<%(tag)s%(attrs)s%(slash)s>" % {
+            "tag": tag,
+            "attrs": (" " + " ".join(attribs)) if len(attribs) > 0 else "",
+            "slash": "/" if tag in HTMLNormalizer.VOID_ELEMENTS else "",
+        }
+        print(line, end="")
+
+    def handle_endtag(self, tag):
+        print(f"</{tag}>", end="")
+
+    def handle_startendtag(self, tag, attrs):
+        self.handle_starttag(tag, attrs)
+
+    def handle_data(self, data):
+        print(html_escape(data), end="")
+
+    def error(self, message):
+        """Ignore errors."""
+
+
+def html_to_xhtml(document):
+    out = StringIO()
+    normalizer = HTMLNormalizer()
+    with redirect_stdout(out):
+        normalizer.feed(document)
+    return out.getvalue()
+
+
 def refs(content):
-    content = content.replace('script defer', 'script')
-    root = ElementTree.fromstring(content)
+    root = ElementTree.fromstring(html_to_xhtml(content))
     return [
         ref.get(attr)
         for tag, attr in (("link", "href"), ("script", "src"), ("img", "src"))
