862c59ff1ed3f0c07ba9877ef4975a5ad0879dbe
H. Turgut Uyar <uyar@tekir.org>
2022-10-20 11:14:31 +0300

    use importlib instead of pkg_resources

--- tests/test_kirlent.py
+++ tests/test_kirlent.py
@@ -1,7 +1,13 @@
-from pkg_resources import get_distribution
+import sys
+
+
+if sys.version_info < (3, 8):
+    import importlib_metadata as metadata
+else:
+    from importlib import metadata
 
 import kirlent
 
 
 def test_installed_version_should_match_tested_version():
-    assert get_distribution("kirlent").version == kirlent.__version__
+    assert metadata.version("kirlent") == kirlent.__version__
